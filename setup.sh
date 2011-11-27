55bc2ada9107f8d7ebf52af84ca244a048e77492
Drew Buschhorn <drewbuschhorn@gmail.com>
2011-11-27 02:05:16 -0500

    Updated dependency setup and removed sunburnt code ... never needed to modify it anyway. Monkey patching in PLOS works well enough.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-####
+#sudo easy_install virtualenv
+#virtualenv my_doctormoon --no-site-packages
+#cd my_doctormoon
+#source bin/activate
+#http://mitchfournier.com/2010/06/25/getting-started-with-virtualenv-isolated-python-environments/
+#git clone git@github.com:drewbuschhorn/DoctorMoon.git
+
+#### Easy dependencies
 ../bin/easy_install django psycopg2 #Doctor Moon ( django site ) dependencies
 ../bin/easy_install twisted psycopg2
 ../bin/easy_install sunburnt httplib2 lxml #Doctor Moon ( twisted site ) dependencies 
+####
 
-## Challenging dependencies
+#### Challenging dependencies
 ../bin/easy_install egenix-mx-base
 ##apt-get install libgraphviz-dev
 ../bin/easy_install pygraphviz 
 ../bin/easy_install numpy 
 ../bin/easy_install matplotlib #Doctor Moon ( twisted site ) dependencies
+####
