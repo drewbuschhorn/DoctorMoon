14f67b6546309158f9d5df2e8b6372584fed397f
Drew Buschhorn <drewbuschhorn@gmail.com>
2011-11-27 22:25:21 -0500

    Adding more setup details to setup.sh for wiki inclusion.

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@
 #source bin/activate
 #http://mitchfournier.com/2010/06/25/getting-started-with-virtualenv-isolated-python-environments/
 #git clone git@github.com:drewbuschhorn/DoctorMoon.git
+#cd DoctorMoon
 
 #### Easy dependencies
 ../bin/easy_install django psycopg2 #Doctor Moon ( django site ) dependencies
@@ -20,3 +21,18 @@
 ../bin/easy_install numpy 
 ../bin/easy_install matplotlib #Doctor Moon ( twisted site ) dependencies
 ####
+
+
+#### Setup Postgres Django database 
+# ( database name = drmoon, username="postgres", password="django13" )
+
+#cd drmoon_project/
+#../../bin/python manage.py syncdb
+# provide superuser
+
+#http://localhost:8000/accounts/register
+# provide test user
+
+#http://localhost:8000/accounts/login
+#http://localhost:8000/networkgraphs/form
+# doi:[10.1371/journal.pmed.0020124]
