6576cdd1b5dcc72e30ac7b4edc5edc9c08bc48a5
Drew Buschhorn <drewbuschhorn@gmail.com>
2012-01-16 14:45:43 -0500

    Fixing profile generation

--- drmoon_project/drmoon/models.py
+++ drmoon_project/drmoon/models.py
@@ -2,18 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 import random
 
+def _generate_request_code():
+    random.seed()
+    nine_digit_number = unicode(random.randint(0,999999999))
+    print nine_digit_number
+    return u'00x' + nine_digit_number.rjust(9,u'0')
+
 class UserProfile(models.Model):
   user = models.ForeignKey(User,unique=True)
-
   created  = models.DateTimeField(auto_now_add = True)
   modified = models.DateTimeField(auto_now = True)
   expired  = models.DateTimeField(null = True, blank = True)
 
-  def _generate_request_code():
-    nine_digit_number = unicode(random.randint(0,999999999))
-    return u'00x' + nine_digit_number.rjust(9,u'0')
-
-  request_code = models.CharField(max_length=12,unique=True,default=_generate_request_code())
+  request_code = models.CharField(max_length=12,unique=True)
 
 ## Extra Signal hook
 from django.db.models.signals import post_save
@@ -24,6 +25,8 @@ def create_profile(sender, instance, created, **kwargs):
     """Create a matching profile whenever a user object is created."""
     if created: 
         profile, new = UserProfile.objects.get_or_create(user=instance)
+	profile.request_code=_generate_request_code()
+	UserProfile.save(profile)
 ## End UserProfile Class
 
 
