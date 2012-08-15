ef86d90ee30dadf5fa386f0f54826d06a8894775
Drew Buschhorn <drewbuschhorn@gmail.com>
2012-08-14 23:24:12 -0400

    Postgres database setup script.

--- drmoon_project/settings.py
+++ drmoon_project/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'drmoon',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'django13',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
--- setup.sh
+++ setup.sh
@@ -18,9 +18,19 @@ pip install -r requirements.txt
 
 #### Setup Postgres Django database 
 # ( database name = drmoon, username="postgres", password="django13" )
+echo "Creating the postgres database. May take a few seconds"
+sudo -u postgres createdb -E UTF-8 drmoon
 
-#cd drmoon_project/
-#../../bin/python manage.py syncdb
+echo "Setting the postgress user password.  Recommend django13 for dev password, but this is highly insecure.  Will also reset any other application using postfres on this machine."
+echo "Proceed? [Y/N]"
+read a
+if [[ $a == "Y" || $a == "y" || $a = "" ]]; then
+	sudo -u postgres psql <<< "\password postgres"
+fi
+
+
+cd drmoon_project/
+python manage.py syncdb
 # provide superuser
 
 #http://localhost:8000/accounts/register
